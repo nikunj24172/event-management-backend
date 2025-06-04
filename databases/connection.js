920457464a4b7c5a0278a8138566f2325d996568
DESKTOP-EH231EQ\DELL <nikunjchudasama1223@gmail.com>
2025-06-04 19:52:05 +0530

    connection changes

--- databases/connection.js
+++ databases/connection.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("database connected successfully...");
   } catch (error) {
     console.error(error);
